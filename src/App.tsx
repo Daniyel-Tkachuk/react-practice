fe15ac371e9b9f47c5484249d494c4e2fcb7f59e
Daniyel Tkachuk <danilaradyn7357899@gmail.com>
2024-04-19 16:41:23 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <div className="App">
             <Rating value={3}/>
             <Accordion title="Menu" collapsed={false}/>
-            <Accordion title="Users" collapsed={true}/>
+            <Accordion title="users" collapsed={true}/>
         </div>
     );
 }
