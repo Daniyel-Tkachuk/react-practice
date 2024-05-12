b997cf9150a58188027c4be555cb51648c9e80a8
Daniyel Tkachuk <danilaradyn7357899@gmail.com>
2024-05-12 17:17:03 +0300

    added component Accordion in StoryBook

--- src/components/accordion/Accordion.stories.tsx
+++ src/components/accordion/Accordion.stories.tsx
@@ -1,18 +1,27 @@
-import type { Meta } from '@storybook/react';
+import type {Meta} from '@storybook/react';
 
-import { Accordion } from './Accordion';
+import {Accordion} from './Accordion';
+import {useState} from "react";
 
 const meta: Meta<typeof Accordion> = {
    component: Accordion,
 };
 
+
 export default meta;
 
 export const CollapsedAccordion = () => {
-   return   <div>collapsed</div>
+   return <Accordion title="Collapsed Accordion" collapsed={true} setCollapsed={() => {}}/>
 }
 
-export const OpenAccordion = () => {
-   return   <div>open</div>
+export const OpenedAccordion = () => {
+   return <Accordion title="Opened Accordion" collapsed={false} setCollapsed={() => {}}/>
 }
+
+export const AccordionDemo = () => {
+   const [collapsed, setCollapsed] = useState<boolean>(false);
+
+   return <Accordion title="Opened Accordion" collapsed={collapsed} setCollapsed={() => setCollapsed(!collapsed)}/>
+}
+
 
