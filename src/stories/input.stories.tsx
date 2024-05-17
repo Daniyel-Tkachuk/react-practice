7a78792f0a4e3b58cdffc182fa2e83648573db86
Daniyel Tkachuk <danilaradyn7357899@gmail.com>
2024-05-17 21:53:49 +0300

    refactor

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,4 +1,6 @@
 import React, {ChangeEvent} from "react";
+import { action } from '@storybook/addon-actions';
+
 
 export default {
    title: "input"
@@ -46,3 +48,43 @@ export const GetValueOfUncontrolledInput = () => {
 export const ControlledInputWithFixedValue = () => {
    return <input type="text" value="it-incubator"/>
 };
+
+export const ControlledInput = () => {
+   const [parentValue, setParentValue] = React.useState("");
+
+   const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+      setParentValue(e.currentTarget.value);
+      action("onChangeHandler")(e.currentTarget.value);
+   }
+
+   return <input type="text" value={parentValue} onChange={onChangeHandler}/>
+}
+
+export const controlledCheckbox = () => {
+   const [checked, setChecked] = React.useState(false);
+
+   const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+      setChecked(e.currentTarget.checked);
+      action("onChangeHandler")(e.currentTarget.checked);
+   }
+
+   return <input type="checkbox" checked={checked} onChange={onChangeHandler}/>
+}
+
+export const controlledSelect = () => {
+   const [options, setOptions] = React.useState<string | undefined>("2");
+
+   const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+      setOptions(e.currentTarget.value);
+      action("onChangeHandler")(e.currentTarget.value);
+   }
+
+   return (
+      <select value={options} onChange={onChangeHandler}>
+         <option value="">none</option>
+         <option value="1">Минск</option>
+         <option value="2">Гродно</option>
+         <option value="3">Гомель</option>
+      </select>
+   )
+}
