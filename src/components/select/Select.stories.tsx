1274f4f2700b2506490e3fcd776a1b6f6230b4fd
Daniyel Tkachuk <danilaradyn7357899@gmail.com>
2024-05-24 16:56:55 +0300

    refactor

--- src/components/select/Select.stories.tsx
+++ src/components/select/Select.stories.tsx
@@ -1,24 +1,59 @@
+import React, {useState} from 'react';
 import {Select} from "./Select";
 import {action} from "@storybook/addon-actions";
+import {Meta, StoryObj} from "@storybook/react";
+import {useArgs} from '@storybook/preview-api';
+import {fn} from "@storybook/test";
+
+/*items={}*/
+
+const meta = {
+   tags: ['autodocs'],
+   component: Select,
+   args: {
+      value: "2",
+      onChange: action('asd'),
+      items: []
+   }
+} satisfies Meta<typeof Select>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
 
 
-export default {
-   title: "Select",
-   component: Select
-}
+export const WithValue = {
+   args: {
+      items: [
+         {value: "1", title: "Minsk"},
+         {value: "2", title: "Гродно"},
+         {value: "3", title: "Витебск"},
+         {value: "4", title: "Могилев"},
+      ],
+   },
+   render: function Render(args) {
+      const [{ value }, setValue] = useArgs();
 
-export const WithValue = () => <Select value="2" onChange={action("Value changed")}
-                                        items={[
-                                           {value: "1", title: "Minsk"},
-                                           {value: "2", title: "Гродно"},
-                                           {value: "3", title: "Витебск"},
-                                           {value: "4", title: "Могилев"},
-                                        ]}/>
+      function onChange(newValue: any) {
+         setValue({ value: newValue });
+         action('select-value')(newValue)
+      }
+
+      return <Select {...args} onChange={onChange} value={value}/>
+   },
+} satisfies Story;
+
+
+
+
+
+
+
 
 export const WithoutValue = () => <Select onChange={action("Value changed")}
-                                       items={[
-                                          {value: "1", title: "Minsk"},
-                                          {value: "2", title: "Гродно"},
-                                          {value: "3", title: "Витебск"},
-                                          {value: "4", title: "Могилев"},
-                                       ]}/>
+                                          items={[
+                                             {value: "1", title: "Minsk"},
+                                             {value: "2", title: "Гродно"},
+                                             {value: "3", title: "Витебск"},
+                                             {value: "4", title: "Могилев"},
+                                          ]}/>
--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -8,23 +8,34 @@ type ItemType = {
 
 type Props = {
    value?: string
-   onChange: () => void
+   onChange: (value: any) => void
    items: ItemType[]
 }
 
 export const Select: FC<Props> = (props) => {
-   const {items, value} = props;
+   const {items, value, onChange} = props;
+
+   const [active, setActive] = React.useState<boolean>(false);
+
+   const toggleItems = () => {
+      setActive(!active);
+   }
+
+   const itemClicked = (value: any) => {
+      onChange(value);
+   }
 
    const selectedItem = items.find(item => item.value === value);
 
    return (
       <div className={s.select}>
-         <h3>{selectedItem && selectedItem.title}</h3>
-         {items.map(el => {
-            return (
-               <div key={el.value}>{el.title}</div>
-            )
-         })}
+         <span className={s.main} onClick={toggleItems}>{selectedItem && selectedItem.title}</span>
+         {
+            active &&
+             <div className={s.items}>
+                {items.map(el => <div key={el.value} onClick={() => onChange(value)}>{el.title}</div>)}
+             </div>
+         }
       </div>
    );
 };
