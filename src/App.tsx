700f0821c6bc348038b0aaf3f001e332b0a8b3c0
Daniyel Tkachuk <danilaradyn7357899@gmail.com>
2024-05-25 22:30:16 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,16 @@ import {UncontrolledOnOff} from "./components/onOff/UncontrolledOnOff";
 import {UnControlledAccordion} from "./components/accordion/UnControlledAccordion";
 import {OnOff} from "./components/onOff/OnOff";
 import {UncontrolledRating} from "./components/rating/UncontrolledRating";
+import {Select} from "./components/select/Select";
 
 
 function App() {
    const [value, setValue] = React.useState<RatingValueType>(2);
    const [collapsed, setCollapsed] = React.useState<boolean>(true);
    const [includedValue, setIncludedValue] = React.useState<boolean>(false);
+   const [selectValue, setSelectValue] = React.useState("2");
 
-   const stateForAccordion: ItemType[] = [
+   const state: ItemType[] = [
       {title: "Минск", value: "1"},
       {title: "Гродно", value: "2"},
       {title: "Витебск", value: "3"},
@@ -29,17 +31,21 @@ function App() {
          <hr/>
          Controlled
          <Accordion title="Controlled"
-                    items={stateForAccordion}
+                    items={state}
                     onClick={value => {console.log(value)}}
                     collapsed={collapsed}
                     setCollapsed={() => setCollapsed(!collapsed)}/>
          Uncontrolled
-         <UnControlledAccordion title="Uncontrolled" items={stateForAccordion}/>
+         <UnControlledAccordion title="Uncontrolled" items={state}/>
          <hr/>
          Controlled
          <OnOff includeValue={includedValue} setIncludeValue={setIncludedValue}/>
          Uncontrolled
          <UncontrolledOnOff/>
+         <hr/>
+         Select
+         <Select onChange={setSelectValue} items={state} value={selectValue}/>
+         <hr/>
       </div>
    );
 }
